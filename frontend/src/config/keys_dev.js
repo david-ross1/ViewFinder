ba31a2d695ab3e63c799b1dcdf1b2d9d7e28a3d8
dross <davidr7711@gmail.com>
2021-06-15 15:19:48 -0700

    .gitignore

--- frontend/src/config/keys_dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb+srv://dev:VTufCZrLyHuY0SeU@cluster0.fxtpq.mongodb.net/ViewFinder?retryWrites=true&w=majority",
-  secretOrKey: "UP6|mSczZLNbE9H",
-  awsAccessKeyId: "AKIAVRTYTFGPN4YAWO6P",
-  awsSecretAccessKey: "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",
-  awsBucketName: "view-finder",
-  awsRegion: "us-west-1",
-};
