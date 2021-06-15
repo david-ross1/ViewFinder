920065c8f9e2ede25e143c316b8e25b445c6aadf
dross <davidr7711@gmail.com>
2021-06-14 22:46:28 -0700

    Test react s3 uploading

---
+++ frontend/src/components/photoapp2.js
@@ -0,0 +1,17 @@
+import React , { Component } from 'react'; 
+
+class Photoapp2 extends Component {
+  constructor() {
+    super(); 
+  }
+  render() {
+    return (
+      <div>
+        <h3>aws s3 upload</h3>
+        
+      </div>
+    )
+  }
+}
+
+export default Photoapp2; 
