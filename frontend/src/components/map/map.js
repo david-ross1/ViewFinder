9e24a94ab1230b29e951aa8103eda08827388c90
dross <davidr7711@gmail.com>
2021-06-15 22:23:53 -0700

    Heroku mapbox access token push

--- frontend/src/components/map/map.js
+++ frontend/src/components/map/map.js
@@ -1,8 +1,8 @@
 import React, {useEffect, useRef, useState} from 'react';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import mapboxgl from 'mapbox-gl';
-
-mapboxgl.accessToken = require("../../config1/keys").mapboxAPI;
+const keys = require("../../config1/keys");
+mapboxgl.accessToken = keys.mapboxAPI;
 
 const MAX_ZOOM = 16;
 const MIN_ZOOM = 7;
