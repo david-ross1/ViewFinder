b8bbc7fd9c74a71ec9bf9f3d110bcbf8761939ba
dross <davidr7711@gmail.com>
2021-06-19 17:56:14 -0700

    heroku

--- frontend/src/components/session/login_form.js
+++ frontend/src/components/session/login_form.js
@@ -66,59 +66,144 @@ class LoginForm extends React.Component {
 
   render() {
     return (
-      <div className='login-form-container-container'>
       <div className="login-form-container">
-        <div className="login-form">
-          <div className="login-head">
-            <button className="sign-button" onClick={this.otherForm}>
-              <p className="button-text">Sign Up</p>
-            </button>
-            <button className="close-button" onClick={this.props.closeModal}>
-              X
-            </button>
-          </div>
-          <br />
-          <br />
-          <form className="session-form" onSubmit={this.handleSubmit}>
+        <div className="login-head">
+          <button className="sign-button" onClick={this.otherForm}>
+            <p className="button-text">Sign Up</p>
+          </button>
+          <button className="close-button" onClick={this.props.closeModal}>
+            X
+          </button>
+        </div>
+        <form className="session-form-sign" onSubmit={this.handleSubmit}>
+          {this.renderErrors()}
+          <div className="login-form">
             <div className="input">
-              <label>Email:</label>
+              <label></label>
               <br />
               <input
+                className="login-text"
                 type="text"
                 value={this.state.email}
                 onChange={this.update("email")}
                 placeholder="Email"
+                required
               />
             </div>
             <br />
-            <br />
-            <div>
-              <label>Password:</label>
+            {/* <div className="input">
+              <label></label>
               <br />
               <input
+                className="login-text"
+                type="text"
+                value={this.state.name}
+                onChange={this.update("name")}
+                placeholder="Name"
+                required
+              />
+            </div> */}
+            <br />
+            <div className="input">
+              <label></label>
+              <br />
+              <input
+                className="login-text"
                 type="password"
                 value={this.state.password}
                 onChange={this.update("password")}
                 placeholder="Password"
+                required
               />
+            </div>
+            <br />
+            {/* <div className="input">
+              <label></label>
               <br />
-            </div>
+              <input
+                className="login-text"
+                type="password"
+                value={this.state.password2}
+                onChange={this.update("password2")}
+                placeholder="Confirm Password"
+                required
+              />
+            </div> */}
             <br />
-            <br />
-            <input className="submit" type="submit" value="Submit" />
+            <input className="submit button" type="submit" value="Submit" />
             {this.renderErrors()}
-            <br />
+            <br />{" "}
             <div className="demo-button">
+              {" "}
               <button onClick={this.demoUser} className="demo-submit">
-                Demo Login
-              </button>
+                Demo Login{" "}
+              </button>{" "}
             </div>
-          </form>
-        </div>
-      </div>
+          </div>
+        </form>
       </div>
     );
   }
 }
 
 export default withRouter(LoginForm);
+//   render() {
+//     return (
+//       <div className="login-form-container">
+//         <div className="login-form">
+//           <div className="login-head">
+//             <button className="sign-button" onClick={this.otherForm}>
+//               <p className="button-text">Sign Up</p>
+//             </button>
+//             <button className="close-button" onClick={this.props.closeModal}>
+//               X
+//             </button>
+//           </div>
+//           <br />
+//           <br />
+//           <form className="session-form" onSubmit={this.handleSubmit}>
+//             <div className="input">
+//               <label></label>
+//               <br />
+//               <input
+//                 className="login-text"
+//                 type="text"
+//                 value={this.state.email}
+//                 onChange={this.update("email")}
+//                 placeholder="Email"
+//                 required
+//               />
+//             </div>
+//             <br />
+//             <br />
+//             <div>
+//               <label></label>
+//               <br />
+//               <input
+//                 className="login-text"
+//                 type="password"
+//                 value={this.state.password}
+//                 onChange={this.update("password")}
+//                 placeholder="Password"
+//                 required
+//               />
+//               <br />
+//             </div>
+//             <br />
+//             <br />
+//             <input className="submit" type="submit" value="Submit" />
+//             {this.renderErrors()}
+//             <br />
+//             <div className="demo-button">
+//               <button onClick={this.demoUser} className="demo-submit">
+//                 Demo Login
+//               </button>
+//             </div>
+//           </form>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default withRouter(LoginForm);
--- frontend/src/components/session/signup_form.js
+++ frontend/src/components/session/signup_form.js
@@ -13,21 +13,20 @@ class SignupForm extends React.Component {
       errors: {},
     };
 
-    this.otherForm = this.otherForm.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.clearedErrors = false;
+    this.otherForm = this.otherForm.bind(this);
+    this.alert = this.alert.bind(this);
+    this.login = this.login.bind(this);
   }
 
-  // componentWillReceiveProps(nextProps) {
-  //   if (nextProps.signedIn === true) {
-  //     this.props.history.push('/');
-  //   }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.signedIn === true) {
+      this.props.otherForm();
+      this.alert();
+    }
 
-  //   this.setState({errors: nextProps.errors})
-  // }
-
-  otherForm() {
-    this.props.otherForm();
+    this.setState({ errors: nextProps.errors });
   }
 
   update(field) {
@@ -46,8 +45,7 @@ class SignupForm extends React.Component {
       password2: this.state.password2,
     };
 
-    this.props.signup(user).then(this.props.closeModal());
-    return alert("User Created");
+    this.props.signup(user, this.props.history);
   }
 
   renderErrors() {
@@ -60,65 +58,81 @@ class SignupForm extends React.Component {
     );
   }
 
+  otherForm() {
+    this.props.otherForm();
+  }
+
+  alert() {
+    return alert("User Created");
+  }
+
   render() {
     return (
       <div className="login-form-container">
         <div className="login-head">
           <button className="sign-button" onClick={this.otherForm}>
-            <p className="button-text">Sign Up</p>
+            <p className="button-text">Login</p>
           </button>
           <button className="close-button" onClick={this.props.closeModal}>
             X
           </button>
         </div>
         <form className="session-form-sign" onSubmit={this.handleSubmit}>
+          {this.renderErrors()}
           <div className="login-form">
             <div className="input">
-              <label>Email:</label>
+              <label></label>
               <br />
               <input
+                className="login-text"
                 type="text"
                 value={this.state.email}
                 onChange={this.update("email")}
                 placeholder="Email"
+                required
               />
             </div>
             <br />
             <div className="input">
-              <label>Name:</label>
+              <label></label>
               <br />
               <input
+                className="login-text"
                 type="text"
                 value={this.state.name}
                 onChange={this.update("name")}
                 placeholder="Name"
+                required
               />
             </div>
             <br />
             <div className="input">
-              <label>Password:</label>
+              <label></label>
               <br />
               <input
+                className="login-text"
                 type="password"
                 value={this.state.password}
                 onChange={this.update("password")}
                 placeholder="Password"
+                required
               />
             </div>
             <br />
             <div className="input">
-              <label>Re-enter password:</label>
+              <label></label>
               <br />
               <input
+                className="login-text"
                 type="password"
                 value={this.state.password2}
                 onChange={this.update("password2")}
                 placeholder="Confirm Password"
+                required
               />
             </div>
             <br />
-            <input className="submit" type="submit" value="Submit" />
-            {this.renderErrors()}
+            <input className="submit button" type="submit" value="Submit" />
           </div>
         </form>
       </div>
