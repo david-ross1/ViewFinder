a9b11890e2c6ca56ee1eee99f0f19fef3d427c9f
dross <davidr7711@gmail.com>
2021-06-19 17:01:46 -0700

    heroku

--- frontend/src/config1/keys.js
+++ frontend/src/config1/keys.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./keys_prod');
 } else {
-  module.exports = require('./keys_dev1');
+  module.exports = require('./keys_dev');
 }
 
