50f294b05bd41da954c35ba61c071339501bcb4b
mohammad <m.a.taghva@gmail.com>
2021-06-21 11:04:12 -0700

    Final css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   // document.body.style = "background: #B6A328";
   document.body.style =
-    "background-image: url(https://view-finder.s3.us-west-1.amazonaws.com/gradient-unsplash.png";
+    // "background-image: url(https://view-finder.s3.us-west-1.amazonaws.com/gradient-unsplash.png";
     // "background-image: url(https://view-finder.s3.us-west-1.amazonaws.com/01_SF_GoldenGate.JPG";
  
 	
