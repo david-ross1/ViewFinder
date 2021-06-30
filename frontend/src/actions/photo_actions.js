99f8a9f98c7e7a4b65d3870c47c5cf8033204684
kerapace <jhill01@manhattan.edu>
2021-06-30 13:27:57 -0400

    created backend routes for photo upload / deletion

--- frontend/src/actions/photo_actions.js
+++ frontend/src/actions/photo_actions.js
@@ -1,2 +1,14 @@
-import {receiveFullViewData} from "./view_actions";
+import {fetchView} from "./view_actions";
+import * as PhotoApiUtil from "../util/photo_api_util";
 
+export const uploadPhoto = photoData => dispatch => 
+  PhotoApiUtil.addPhoto(photoData).then(res => {
+    const { viewId } = res.data;
+    dispatch(fetchView(viewId));
+  });
+
+export const deletePhoto = photoData => dispatch =>
+  PhotoApiUtil.deletePhoto(photoData).then(res => {
+    const { viewId } = res.data;
+    dispatch(fetchView(viewId));
+  });
