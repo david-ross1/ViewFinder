cf36816c537f87cfe9468426100b76595f83807a
kerapace <jhill01@manhattan.edu>
2021-07-06 21:39:12 -0400

    cleanup

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { AuthRoute, ProtectedRoute } from "../util/route_util";
-import { Switch, Route } from "react-router-dom";
+// import { AuthRoute, ProtectedRoute } from "../util/route_util";
+import { Route } from "react-router-dom";
 import NavBarContainer from "./nav/navbar_container";
 
 import MainPage from "./main/main_page";
 import Footer from "./footer/footer"
 import Modal from "./modal/modal"
-
-
-import PhotoApp2 from './photoapp2'
 
 const App = () => (
   <div>
